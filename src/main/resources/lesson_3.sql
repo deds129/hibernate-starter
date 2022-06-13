ce216e8da83f28c412d30b2b96e71ea87e56bdd5
deds129 <nik.chudinov129@mail.ru>
2022-06-13 21:48:52 +0300

    [3_lesson] set up project properties + create db

---
+++ src/main/resources/lesson_3.sql
@@ -0,0 +1,9 @@
+create table users
+(
+    username   varchar(128) not null
+        primary key,
+    firstname  varchar(128),
+    lastname   varchar(128),
+    birth_date date,
+    age        integer
+);
