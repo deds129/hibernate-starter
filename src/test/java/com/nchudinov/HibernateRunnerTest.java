60124cd8461f2680cc02ad4ad55b4172c399e3df
deds129 <nik.chudinov129@mail.ru>
2022-10-07 17:43:11 +0300

    [7.1_lesson] HQL p1

--- src/test/java/com/nchudinov/HibernateRunnerTest.java
+++ src/test/java/com/nchudinov/HibernateRunnerTest.java
@@ -4,7 +4,34 @@ import com.nchudinov.entity.*;
 import com.nchudinov.util.HibernateTestUtil;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class HibernateRunnerTest {
+	
+	@Test
+	void checkHQL() {
+		try (var sessionFactory = HibernateTestUtil.buildSessionFactory();
+			 var session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			//SQL
+			//select * from users
+			//select u.* from users u where u.firstName = 'Sasha'
+			
+			//all actions such as SQL, operate by object mapping
+			//HQL
+			String name = "nchudinov";
+			List<User> users = session
+					.createQuery(
+							"select u from User u " +
+									"where u.username = :username", User.class)
+					.setParameter("username", name)
+					.list(); //invoke chain patten
+			users.forEach(user -> System.out.println(user.getUsername()));
+
+			session.getTransaction().commit();
+
+		}
+	}
 
 	@Test
 	void checkDockerPostgres() {
