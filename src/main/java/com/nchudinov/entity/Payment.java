1e84b20104a3a61ddc8f77e0b46a60d8cb6392ab
deds129 <nik.chudinov129@mail.ru>
2022-11-28 14:19:12 +0300

    [9.3] Optimistic lock p2

--- src/main/java/com/nchudinov/entity/Payment.java
+++ src/main/java/com/nchudinov/entity/Payment.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.OptimisticLockType;
 import org.hibernate.annotations.OptimisticLocking;
 
@@ -14,15 +15,16 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Builder
 @Entity
-@OptimisticLocking(type = OptimisticLockType.VERSION)
+@OptimisticLocking(type = OptimisticLockType.ALL)
+@DynamicUpdate
 public class Payment implements BaseEntity<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 	
-	@Version
-	private Long version;
+//	@Version
+//	private Long version;
 
     @Column(nullable = false)
     private Integer amount;
