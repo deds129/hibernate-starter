fb1778f3761814c423265ec84c7df8445e45e1ec
deds129 <nik.chudinov129@mail.ru>
2022-11-28 12:19:05 +0300

    [9.3] Optimistic lock p1

--- src/main/java/com/nchudinov/HibernateRunner.java
+++ src/main/java/com/nchudinov/HibernateRunner.java
@@ -2,37 +2,36 @@ package com.nchudinov;
 
 import com.nchudinov.entity.Payment;
 import com.nchudinov.util.HibernateUtil;
+import com.nchudinov.util.TestDataImporter;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.jpa.QueryHints;
 
-import javax.persistence.QueryHint;
+import javax.persistence.LockModeType;
 import javax.transaction.Transactional;
-import java.sql.SQLException;
 
 @Slf4j
 public class HibernateRunner {
 
 	@Transactional
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args)  {
 		try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-			 Session session = sessionFactory.openSession()) {
+			 Session session = sessionFactory.openSession();
+			 Session session2 = sessionFactory.openSession();) {
 			
-			// 0 - нативный способ
-			session.createNativeQuery("set transaction read only").executeUpdate();
-			
-			// 1
-			session.setDefaultReadOnly(true);
+			TestDataImporter.importData(sessionFactory);
 			session.beginTransaction();
-			var payment = session.createQuery("select p from Payment p", Payment.class)
-					// 2
-							.setReadOnly(true) //предпочтительный способ
-					// 3
-									.setHint(QueryHints.HINT_READONLY, true);
-			// 4
-			session.setReadOnly(payment, true);
+			session2.beginTransaction();
+			
+			//Optimistic lock
+			var payment = session.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+			payment.setAmount(payment.getAmount() * 2);
+			
+			var payment2 = session.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+			payment2.setAmount(payment2.getAmount() * 30);
+
 			session.getTransaction().commit();
+			session2.getTransaction().commit();
 
 		}
 	}
