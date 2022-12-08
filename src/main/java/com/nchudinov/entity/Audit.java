28fc2a554630fa8a7e5520def6cced6b78f6eaa8
deds129 <nik.chudinov129@mail.ru>
2022-12-08 13:52:25 +0300

    [10.3] Event Listeners

--- src/main/java/com/nchudinov/entity/Audit.java
+++ src/main/java/com/nchudinov/entity/Audit.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.lang.invoke.SerializedLambda;
 
@@ -29,6 +26,7 @@ public class Audit {
 	
 	private String entityContent;
 	
+	@Enumerated(EnumType.STRING)
 	private Operation operation;
 
 	public enum Operation {
